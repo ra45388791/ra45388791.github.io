966b28c15f33f16e5cd41f3f85b6171690200ede
Zhang Rong Zhan <ra45388791@gmail.com>
2022-01-12 22:46:31 +0800

    1

--- script/index.js
+++ script/index.js
@@ -1,14 +1,45 @@
 let data = {
-    message: 'is true!',
-    a: 1,
-    divdom: '<div>這是一個測試的div</div>'
+    isTrueOrFalse: '',                       //v-model 選項輸入測試
+    message: 'is true!',                    //第一行訊息
+    message2: '倒轉插值',                   //倒轉插值
+    color: '',                              //顏色
+    divdom: '<div>這是一個測試的div</div>', //圖片
+    a: 1,                                   //watch測試
+    seeIf: true,                            //v-if 測試
+    vueInput: ''                            //v-model 表單輸入測試
 }
 // 阻止修改現有屬性 指定某個屬性不能被修改
 // Object.freeze(data);
 
 let app = new Vue({
     el: '#app',
-    data: data
+    data: data,
+    methods: {
+        //v-on 選項切換監控
+        vueSelect: function () {
+            let d = app.$data.isTrueOrFalse;
+
+            if (d === 'true') {
+                data.message = d;
+                //改變字體顏色
+                data.color = 'testRed';
+                // 傳進v-html 中的圖片tag
+                data.divdom = '<img src="img/17d50c0.jpg" alt="">';
+            } else if (d === 'false') {
+                data.message = d;
+                //改變字體顏色
+                data.color = 'testBlue';
+                // 傳進v-html 中的圖片tag
+                data.divdom = '<img src="img/1587963.jpg" alt="">';
+            }
+        },
+        //v-on 點擊事件監控
+        appbtn: function () {
+            app.$data.divdom = `<div>${app.$data.vueInput}</div>`
+
+        }
+
+    }
 });
 
 //觀察 app.data.message中的值 
@@ -19,19 +50,20 @@ app.$watch('divdom', function (newValue, oldValue) {
 
 app.$data.a = 995;
 
-
-let item = document.querySelector('#isTrueOrFalse');
-
-item.addEventListener('change', () => {
-    // console.log(item.value);
-    if (item.value === 'true') {
-        data.message = item.value;
-        // 傳進v-html 中的圖片tag
-        data.divdom = '<img src="img/17d50c0.jpg" alt="">';
-    } else if (item.value === 'false') {
-        data.message = item.value;
-        // 傳進v-html 中的圖片tag
-        data.divdom = '<img src="img/1587963.jpg" alt="">';
-    }
-})
-
+// let item = document.querySelector('#isTrueOrFalse');
+// item.addEventListener('change', () => {
+//     // console.log(item.value);
+//     if (item.value === 'true') {
+//         data.message = item.value;
+//         //改變字體顏色
+//         data.color = 'testRed';
+//         // 傳進v-html 中的圖片tag
+//         data.divdom = '<img src="img/17d50c0.jpg" alt="">';
+//     } else if (item.value === 'false') {
+//         data.message = item.value;
+//         //改變字體顏色
+//         data.color = 'testBlue';
+//         // 傳進v-html 中的圖片tag
+//         data.divdom = '<img src="img/1587963.jpg" alt="">';
+//     }
+// })
